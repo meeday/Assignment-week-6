69f2f524ef4eb8589c7de5b8dd75f35c904cf63a
Meeday <meedaxa.ahmed@gmail.com>
2020-04-28 15:38:40 +0100

    formatted file

--- script.js
+++ script.js
@@ -4,11 +4,8 @@
    function search() {
        cityName = $("#cityname").val().toLowerCase();
        $(".week-list").empty();
-       // if function to make sure duplicate values aren't saved to local storage
-       if (itemsArray.indexOf(cityName) == -1) {
-           itemsArray.push(cityName);
-           localStorage.setItem('items', JSON.stringify(itemsArray))
-       };
+       itemsArray.push(cityName);
+       localStorage.setItem('items', JSON.stringify(itemsArray))
        createList();
        $(".weather-side").show();
        $(".info-side").show();
@@ -74,18 +71,18 @@
                url: secondqueryURL,
                method: "GET"
            }).then(function (response) {
-               
+
                // to change the format of the date provided in the response I used the split method to arrange the dates to the standard british notation
                var currentDate = (response.date_iso).split("T")[0]
                var year = (currentDate).split('-')[0]
                var month = (currentDate).split('-')[1]
                var day = ((currentDate).split('-')[2])
                FinalDate = (day + '-' + month + '-' + year)
-               
+
                //append final date to html
                $(".citynamedisplay").html(citynamedisplay + '<br></br>' + FinalDate)
 
-              
+
                //append uv index to the corresponding html element
                $(".uvindex").text(response.value);
                // to change response.value into a number and then round it to the nearest integer
@@ -125,20 +122,20 @@
                url: thirdqueryURL,
                method: "GET"
            }).then(function (response) {
-               
+
                var fivedaysweather = response.list;
                // i = 8 in the for loop because the api used is 5 day/ 3 hour forecast data 24hrs/3 = 8 sets of data per day
                // this was done to make sure that the data given is the next days instead of 3 hours from when the Ajax call was made
                for (var i = 8; i < fivedaysweather.length; i = i + 7) {
-                   
+
                    //to change the date format 
                    var currentDate = (fivedaysweather[i].dt_txt).split(' ')[0];
-                   
+
                    var year = (currentDate).split('-')[0]
                    var month = (currentDate).split('-')[1]
                    var day = ((currentDate).split('-')[2])
                    FinalDate = (day + '-' + month + '-' + year);
-                   
+
                    // to generate the div that will contain the five day forecast
                    var newDiv = $("<div>");
                    newDiv.addClass("col forecast");
@@ -158,8 +155,8 @@
 
    // to get searches from local storage that were saved and display them in the searches list
    var itemsArray = JSON.parse(localStorage.getItem('items')) || [];
-   var lastSearch = itemsArray[itemsArray.length-1];
-console.log(lastSearch)
+   var lastSearch = itemsArray[itemsArray.length - 1];
+   console.log(lastSearch)
    // create a list of locations already searched
    for (var i = 0; i < itemsArray.length && i < 8; i++) {
        var list = $("<p>").text(itemsArray[i])
